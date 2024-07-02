52f638b1d17faa55ba2872574da8222b8f19fed7
ReshabPrasad <153843058+ReshabPrasad@users.noreply.github.com>
2024-07-02 21:36:49 +0530

    Login-UI done with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css'
+import Login from './pages/auth/Login'
 
 function App() {
 
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <Login/>
+    </>
   )
 }
 
---
+++ src/pages/auth/Login.jsx
@@ -0,0 +1,27 @@
+function Login(){
+    return(
+        <div className="flex justify-center items-center h-[90vh]">
+            <div className="card bg-primary text-primary-content w-96">
+                <div className="card-body flex flex-col items-center">
+                    <div className="w-full flex justify-center">
+                        <h2 className="card-title text-4xl text-white">Login</h2>
+                    </div>
+                    <div className="w-full">
+                        <input autoComplete="one-time-code" type="text" placeholder="User-id..." className="input text-white input-bordered input-primary w-full max-w-xs" />
+                    </div>
+                    <div className="w-full">
+                        <input autoComplete="one-time-code" type="password" placeholder="Password" className="input text-white input-bordered input-primary w-full max-w-xs" />
+                    </div>
+                    <div className="w-full mt-4">
+                        <div className="card-actions justify-end">
+                        <button className="btn btn-warning w-full font-bold text-xl">SUBMIT</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Login
