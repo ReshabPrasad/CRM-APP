442d3e309a8b33ab1a3421f613292b814490bb19
ReshabPrasad <153843058+ReshabPrasad@users.noreply.github.com>
2024-07-06 19:26:29 +0530

    added line chart

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
-import { ArcElement, Chart as ChartJS, Legend, Title, Tooltip } from "chart.js";
-import { Pie } from "react-chartjs-2";
+import { ArcElement, CategoryScale, Chart as ChartJS, Legend, LinearScale, LineElement, PointElement, Title, Tooltip } from "chart.js";
+import { useEffect, useState } from "react";
+import { Line, Pie } from "react-chartjs-2";
 import { BsFillPencilFill } from "react-icons/bs";
 import { MdCancel } from "react-icons/md";
 import { MdOutlineDoneAll } from "react-icons/md";
@@ -11,7 +12,7 @@ import Usetickets from "../../Hooks/UseTickets";
 import HomeLayout from "../../Layout/HomeLayout";
 
 
-ChartJS.register(ArcElement, Legend, Title, Tooltip);
+ChartJS.register(ArcElement, Legend, Title, Tooltip, CategoryScale, LinearScale,PointElement,LineElement);
 
 
 function Home(){
@@ -19,6 +20,12 @@ function Home(){
 
 
     const [ticketState]= Usetickets();
+
+    const [lineticketsdata,setlineticketsdata] = useState({
+        opentickets:[],
+        inProgresstickets:[],
+        resolvedtickets:[]
+    });
 
     const pieChartData = {
         labels : Object.keys(ticketState.ticketDistribution),
@@ -32,6 +39,70 @@ function Home(){
             }
         ]
      };
+
+
+     const lineChartData = {
+        labels : Object.keys(lineticketsdata.inProgresstickets),
+        fontColor: "white",
+        datasets : [
+            {
+                label: "Open Tickets Data",
+                data : Object.values(lineticketsdata.opentickets),
+                borderColor: 'rgb(255, 99, 132)',
+            },
+            {
+                label: "In Progress Tickets Data",
+                data : Object.values(lineticketsdata.inProgresstickets),
+                borderColor: 'rgb(53, 162, 235)',
+            },
+            {
+                label: "Resolved Tickets Data",
+                data : Object.values(lineticketsdata.resolvedtickets),
+                borderColor: 'rgb(245, 205, 95)',
+            }
+        ]
+     };
+
+     function processTickets(){
+        const currentdate = new Date();
+        const tenthDateFromToday = new Date();
+        tenthDateFromToday.setDate(currentdate.getDate()-10);
+        if(ticketState.ticketlist.length>0){
+            let openticketdata = {};
+            let inprogressticketdata = {};
+            let resolvedtickedata = {};
+
+            for(let i=10;i>=0;i--){
+                const dateobject = new Date();
+                dateobject.setDate(dateobject.getDate()-i);
+                openticketdata[dateobject.toLocaleDateString().split("/").reverse().join("-")] = 0;
+                inprogressticketdata[dateobject.toLocaleDateString().split("/").reverse().join("-")] = 0;
+                resolvedtickedata[dateobject.toLocaleDateString().split("/").reverse().join("-")] = 0;
+            }
+            ticketState.ticketlist.forEach(ticket => {
+                const date = new Date(ticket.createdAt).toLocaleDateString().split("/").reverse().join("-");
+                const ticketDate = new Date(ticket.createdAt);
+                if(ticket.status == "open" && ticketDate>=tenthDateFromToday){
+                    openticketdata[date] =  openticketdata[date]+1;
+                }
+                if(ticket.status == "inProgress" && ticketDate>=tenthDateFromToday){
+                    inprogressticketdata[date] = inprogressticketdata[date]+1;
+                }
+                if(ticket.status == "resolved" && ticketDate>=tenthDateFromToday){
+                    resolvedtickedata[date] = resolvedtickedata[date]+1;
+                }
+            });
+           setlineticketsdata({
+             opentickets:openticketdata,
+             inProgresstickets:inprogressticketdata,
+             resolvedtickets:resolvedtickedata
+           });
+        }
+     }
+
+     useEffect(() => {
+        processTickets();
+     },[ticketState.ticketlist]);
       
        return(
        <HomeLayout>
@@ -90,6 +161,11 @@ function Home(){
             <div className="justify-center flex items-center m-10 gap-10">
             <div className="h-80 w-80">
                 <Pie data = {pieChartData}/>
+            </div>
+            </div>
+            <div className="justify-center flex items-center m-10 gap-10">
+            <div className="w-[50rem] bg-[wheat]">
+                <Line data = {lineChartData}/>
             </div>
             </div>
         </HomeLayout>
