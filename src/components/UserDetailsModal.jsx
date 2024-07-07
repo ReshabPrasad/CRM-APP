e0b6ca47445e70a142da543a1d9148c02733fd60
ReshabPrasad <153843058+ReshabPrasad@users.noreply.github.com>
2024-07-07 20:07:11 +0530

    updating user completed

--- src/components/UserDetailsModal.jsx
+++ src/components/UserDetailsModal.jsx
@@ -9,16 +9,20 @@ function UserDetailsModal({users ,resetTable}){
     const [userdisplay,setuserdisplay] = useState(users);
 
 
-    async function onStatusChange(e){
-        console.log(e.target.textContent);
-        const dropdown = document.getElementById("dropdown-open");
-        dropdown.open=!dropdown.open;
-        toast("Updating the user....");
+    async function onUserChange(e){
+        try {
+            const ul = e.target.parentNode.parentNode;
+            const name = ul.getAttribute("name");
+            console.log(name);
+            const dropdown = document.getElementById(`${name}Dropdown`);
+            const userId = users.userid; 
+            dropdown.open=!dropdown.open;
+            toast("Updating the user....");
         const response =await axiosInstance.patch("/user/updateUser",{
-            userId : userdisplay.userid,
+            userId : userId,
             updates : {
                 ...userdisplay,
-                userStatus:e.target.textContent
+                [name]:e.target.textContent
             },
         },{
             headers : {
@@ -33,9 +37,13 @@ function UserDetailsModal({users ,resetTable}){
                 email : user.email,
                 userStatus: user.userStatus,
                 clientName:user.clientName,
-                userType:user.userdisplay
+                userType:user.userType,
+                id : userId
             });
             resetTable();
+        }
+        } catch (error) {
+            toast.error("Something went wrong");
         }
     }
   return (
@@ -46,17 +54,28 @@ function UserDetailsModal({users ,resetTable}){
                         <p className="py-4">Email : <span className="text-yellow-400">{userdisplay.email}</span></p>
                         <p className="py-4">User Status : 
                             <span className="text-yellow-400">
-                            <details className="dropdown ml-2" id="dropdown-open">
+                            <details className="dropdown ml-2" id="userStatusDropdown">
                             <summary className="btn m-1"  >{userdisplay.userStatus}</summary>
-                            <ul onClick = {onStatusChange} className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-                                <li><a>Approved</a></li>
-                                <li><a>Suspended</a></li>
-                                <li><a>Rejected</a></li>
+                            <ul name="userStatus" onClick = {onUserChange} className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                                <li><a>approved</a></li>
+                                <li><a>suspended</a></li>
+                                <li><a>rejected</a></li>
                             </ul>
                             </details>
                             </span>
                         </p>
-                        <p className="py-4">User Type : <span className="text-yellow-400">{userdisplay.userType}</span></p>
+                        <p className="py-4">User Type : 
+                            <span className="text-yellow-400">
+                            <details className="dropdown ml-2" id="userTypeDropdown">
+                            <summary className="btn m-1"  >{userdisplay.userType}</summary>
+                            <ul name="userType" onClick = {onUserChange} className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                                <li><a>engineer</a></li>
+                                <li><a>admin</a></li>
+                                <li><a>customer</a></li>
+                            </ul>
+                            </details>
+                            </span>
+                        </p>
                         <p className="py-4">Client Name : <span className="text-yellow-400">{userdisplay.clientName}</span></p>
                         
                     </div>
